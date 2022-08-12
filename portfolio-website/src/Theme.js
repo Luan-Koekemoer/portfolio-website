9e6a000a4f5ef151715c18c6700d3c2035b9deff
Luan Koekemoer <luankoeke@gmail.com>
2022-08-12 22:46:54 +0200

    core sturcutre

---
+++ portfolio-website/src/Theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@material-ui/core'
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      main: '#f0640f',
+    },
+    primary: {
+      main: '#006400',
+    },
+    secondary: {
+      main: '#ffa500',
+    },
+  },
+})
---
+++ portfolio-website/src/routes/home.jsx
@@ -0,0 +1,11 @@
+import '../styles/App.css'
+
+export default function Home() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>hello luan</h1>
+      </header>
+    </div>
+  )
+}
