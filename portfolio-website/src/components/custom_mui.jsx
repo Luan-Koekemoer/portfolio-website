2ac8c3d1080c324f9337abded7e9c36329387039
Luan Koekemoer <luankoeke@gmail.com>
2022-09-17 15:55:49 +0200

    navlink styling and anchor support

--- portfolio-website/src/components/custom_mui.jsx
+++ portfolio-website/src/components/custom_mui.jsx
@@ -1,6 +1,7 @@
-import { Button } from '@mui/material'
+import { Button, Typography } from '@mui/material'
+import { HashLink } from 'react-router-hash-link'
 import { styled } from '@mui/material/styles'
-import {theme} from '../Theme'
+import { theme } from '../Theme'
 
 export const StyledNavButton = styled(Button, { theme })({
   color: theme.palette.text.primary,
@@ -8,5 +9,41 @@ export const StyledNavButton = styled(Button, { theme })({
   '&:hover': {
     fontWeight: 'bold',
     color: theme.palette.text.focus,
+  },
+})
+
+export const StyledNavTypography = styled(Typography, { theme })({
+  color: theme.palette.text.primary,
+  backgroundColor: theme.palette.primary.main,
+  '&:hover': {
+    fontWeight: 'bold',
+    color: theme.palette.text.focus,
+  },
+})
+
+export const StyledNavHashLink = styled(HashLink, { theme })({
+  color: theme.palette.text.primary,
+  textDecoration: 'none',
+
+  '&:focus': {
+    textDecoration: 'none',
+  },
+
+  '&:hover': {
+    fontWeight: 'bold',
+    color: theme.palette.text.focus,
+    textDecoration: 'none',
+  },
+
+  '&:visited': {
+    textDecoration: 'none',
+  },
+
+  '&:link': {
+    textDecoration: 'none',
+  },
+
+  '&:active': {
+    textDecoration: 'none',
   },
 })
